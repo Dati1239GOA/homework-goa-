d3a627cb668ce3745a9050b24aa74f80c6be3c0d
Dati1239GOA <162359596+Dati1239GOA@users.noreply.github.com>
2024-03-05 21:52:18 +0400

    day 5

---
+++ day 1.py
@@ -0,0 +1,22 @@
+country = "germany"
+print(country)
+
+drink = "beer "
+print(drink)
+ 
+value = " has best "
+print(country + value + drink)
+
+number = 657
+second = 1333
+print(number + second)
+
+
+three = 50
+four = 5
+print(three / four)
+
+
+five = 6
+six = 5
+print(five * six)
